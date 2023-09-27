9c8465543f3c823fb3d1017402f50e57cf96505c
Abderrahmane-Br <abderrahmane.brahim.etd@univ-tiaret.dz>
2023-09-27 23:52:37 +0100

    created initial useFetchData hook

---
+++ src/hooks/useFetchData.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+
+function useFetchData(url) {
+
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null)
+
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+
+                setIsLoading(false);
+                setData(data);
+            })
+            .catch(err => {
+                setIsLoading(false);
+                setError(err);
+            })
+    }, [url])
+
+    return [data, isLoading, error];
+}
+export default useFetchData;
