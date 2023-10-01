7fb618fc48979d26f7a07a1b1b3ad449c39e282e
Abderrahmane-Br <abderrahmane.brahim.etd@univ-tiaret.dz>
2023-10-01 18:17:42 +0100

    added session caching

--- src/hooks/useFetchData.jsx
+++ src/hooks/useFetchData.jsx
@@ -3,28 +3,40 @@ import { useState, useEffect } from "react";
 
 function useFetchData(url, transformer = null) {
 
-    const [data, setData] = useState([]);
+    const [data, setData] = useState(JSON.parse(sessionStorage.getItem("pokemons-list")) || []);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null)
 
 
     useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-
+        let fetchTO = null;
+        if (data.length > 0) {
+            fetchTO = setTimeout(() => {
                 setIsLoading(false);
-                if (transformer)
-                    data = transformer(data);
-                setData(data);
-            })
-            .catch(err => {
-                setIsLoading(false);
-                setError(err);
-            });
-        // setTimeout(() => {
+            }, 2500);
+        }
+        else {
+            fetchTO = setTimeout(() => {
+                fetch(url)
+                    .then(res => res.json())
+                    .then(data => {
 
-        // }, 2500)
+                        setIsLoading(false);
+                        if (transformer)
+                            data = transformer(data);
+                        setData(data);
+                        sessionStorage.setItem("pokemons-list", JSON.stringify(data));
+                    })
+                    .catch(err => {
+                        setIsLoading(false);
+                        setError(err);
+                    });
+            }, 2500);
+        }
+
+        return () => {
+            clearTimeout(fetchTO);
+        }
     }, [url])
 
     return [data, isLoading, error];
